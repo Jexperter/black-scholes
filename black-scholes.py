29a1550ee3e1ab0d4a6163f06785021c3cebd3cc
Jesan Akter <66126851+Jexperter@users.noreply.github.com>
2025-01-17 11:52:44 +0000

    update 1

--- black-scholes.py
+++ black-scholes.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from scipy.stats import norm
 import plotly.graph_objects as go
-from numpy import log, sqrt, exp  # Make sure to import these
+from numpy import log, sqrt, exp  
 import matplotlib.pyplot as plt
 import seaborn as sns
 
